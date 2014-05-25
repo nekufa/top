4df0b73305c18c877ec3fe41a3bccbcfdbb47941
Dmitry Krokhin <nekufa@gmail.com>
2014-05-25 15:14:54 +0400

    Update index.php

--- index.php
+++ index.php
@@ -3,46 +3,54 @@
 $limit = 10;
 $filename = 'data.php';
 
-if(isset($_GET['firstName']) && isset($_GET['lastname']) && isset($_GET['points'])) {
+if(isset($_GET['firstName']) && isset($_GET['lastName']) && isset($_GET['points'])) {
+  
+  $row = array(
+    'firstName' => $_GET['firstName'],
+    'lastName' => $_GET['lastName'],
+    'points' => $_GET['points']
+  );
 
   // intialize file  
   if(!file_exists($filename)) {
     file_put_contents($filename, '<?php return array();');
   }
   
+  
   // lock and read current
   $fp = fopen($filename, "r+");
   while (!flock($fp, LOCK_EX)) {
     usleep(1000); // 1ms sleep
   }
+  
   $data = include $filename;
 
-  // append data
-  $data[] = array(
-    'firstName' => $_GET['firstName'],
-    'lastName' => $_GET['lastName'],
-    'points' => $_GET['points']
-  );
+  $key = json_encode($row);
   
-  // remove min
-  if(count($data) > $limit) {
-    $min = null;
-    $key = null;
-    foreach($data as $k => $row) {
-      if(!$min) {
-        $min = $row['points'];
-        $key = $k;
-      } elseif($row['points'] < $min) {
-        $min = $row['points'];
-        $key = $k;
-      }
-    }
+  if(!isset($data[$key])) {
+    // append data
+    $data[$key] = $row;
     
-    // remove min and fix index
-    unset($data[$key]);
-    $data = array_values($data);
+    // remove min
+    if(count($data) > $limit) {
+      $min = null;
+      $key = null;
+      foreach($data as $k => $row) {
+        if(!$min) {
+          $min = $row['points'];
+          $key = $k;
+        } elseif($row['points'] < $min) {
+          $min = $row['points'];
+          $key = $k;
+        }
+      }
+      
+      // remove min and fix index
+      unset($data[$key]);
+      $data = array_values($data);
+    }      
   }
-  
+
   // put contents via lock
   ftruncate($fp, 0);
   fwrite($fp, '<?php return ' . var_export($data, true).';');
